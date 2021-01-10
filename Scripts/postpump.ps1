afc4b04c1b882bc1051efb0c73d1c64c6d0826ef
jgedne <jgedney@deloitte.com>
2021-01-09 20:21:49 -0500

    add pump script

---
+++ Scripts/postpump.ps1
@@ -0,0 +1,7 @@
+﻿
+foreach($line in Get-Content "C:\Users\jgedney\Documents\sample-output.txt" ) {
+    if($line -match $regex){
+        Invoke-WebRequest -UseDefaultCredentials -Method POST -Body $line -Uri "http://localhost/xAPIReceiver/api/xapi" -ContentType application/json
+        Start-Sleep -Seconds 2
+    }
+}
